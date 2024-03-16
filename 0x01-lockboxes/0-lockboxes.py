9bc9485c80cba25a3200e159c9b0ed91dbd25de3
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-03-16 21:29:50 +0000

    A function that determines if all boxes can be opened, return true if all boxes can be opened otherwise false

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""A script that determines if all boxes can be opened
+from n number of locked boxes."""
+
+from typing import List
+
+
+def canUnlockAll(boxes: List[List]) -> bool:
+    """
+        A function that determines if all boxes can be opened.
+        :param boxes: List of lists.
+        :return: True if all boxes can be opened, otherwise False.
+
+        Description:
+            : A key with the same number as a box opens that box
+            : All keys will be positive integers
+                     : There can be keys that do not have boxes
+            : The first box boxes[0] is unlocked
+     """
+
+    if not boxes:
+        return True
+
+    visited_boxes = set()
+    stack_of_boxes: List[int] = [0]
+
+    while stack_of_boxes:
+        current_box = stack_of_boxes.pop()
+        visited_boxes.add(current_box)
+        keys_list: List[int] = boxes[current_box]  # list of keys [1, 4, 6]
+
+        for key in keys_list:
+            if key not in visited_boxes and key < len(boxes):
+                stack_of_boxes.append(key)
+
+    return len(visited_boxes) == len(boxes)
