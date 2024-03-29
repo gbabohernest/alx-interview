82de409c96fd45a5540d727d5008cad5df9ef00e
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-03-29 00:29:43 +0000

    Add exception handling, raise expection for when the user press ctrl + c to stop the code execution, use try and expect block for better error checking

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,7 +5,6 @@ A Python script that reads stdin line by line and computes metrics
 
 import sys
 import re
-import signal
 
 
 line_count = 0
@@ -24,37 +23,39 @@ def print_statistics():
             print(f'{code}: {status_code_counts[code]}')
 
 
-def signal_handler(sig, frame):
-    """
-    Handle keyboard interruption
-    (CTRL + C)
-    """
-    print_statistics()
-    sys.exit(0)
-
-
-# setup signal handler for CTRL + C
-signal.signal(signal.SIGINT, signal_handler)
-
 # Read from stdin line by line
-for line in sys.stdin:
-    line = line.strip()
+try:
+    for line in sys.stdin:
+        line = line.strip()
 
-    match = re.match(
-        r'^(\S+) - \[.*\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)$', line
-    )
+        try:
 
-    if match:
-        ip_address, status_code, file_size = match.groups()
-        status_code = int(status_code)
-        file_size = int(file_size)
+            match = re.match(
+                r'^(\S+) - \[.*\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)$',
+                line
+            )
 
-        # Update metrics
-        line_count += 1
-        total_file_size += file_size
-        if status_code in status_code_counts:
-            status_code_counts[status_code] += 1
+            if match:
+                ip_address, status_code, file_size = match.groups()
+                status_code = int(status_code)
+                file_size = int(file_size)
 
-        # Print statistics after every 10 lines
-        if line_count % 10 == 0:
-            print_statistics()
+                # Update metrics
+                line_count += 1
+                total_file_size += file_size
+                if status_code in status_code_counts:
+                    status_code_counts[status_code] += 1
+
+                # Print statistics after every 10 lines
+                if line_count % 10 == 0:
+                    print_statistics()
+
+        except ValueError:
+            pass
+        except AttributeError:
+            pass
+
+except KeyboardInterrupt:
+    # Handle keyboard interrupt (CTRL + C)
+    print_statistics()
+    raise
