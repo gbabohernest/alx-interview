26fcc38c3714a50944be48be043bef5be12e412a
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-04-17 23:32:40 +0000

    A script that prints the all characters of a Star War movie

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,41 +1,52 @@
 #!/usr/bin/node
 // A script that prints the all characters of a Star War movie
 
-const request = require('request-promise-native');
+const request = require('request');
 
-const fetchCharacters = async (movieID) => {
+const fetchCharacters = (movieID) => {
   // A function to fetch characters for a specific movieID
 
   try {
     const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
-    const response = await request(url);
-    const movie = JSON.parse(response);
-    // console.log(movie)
-    // console.log(`${movie.title}`);
+    request(url, (error, response, body) => {
+      if (error) {
+        console.log(error);
+        process.exit(1);
+      }
+      const movie = JSON.parse(body);
+      // console.log(movie)
+      // console.log(`${movie.title}`);
 
-    const sortedCharacters = await Promise.all(movie.characters.map(fetchCharacterDetails));
-    sortedCharacters.sort((a, b) => {
-      const getId = (url) => parseInt(url.split('/').slice(-2, -1)[0]);
-      return getId(a.url) - getId(b.url);
-    });
+      const characterPromises = movie.characters.map(characterURL => fetchCharacterDetails(characterURL));
 
-    sortedCharacters.forEach(character => {
-      console.log(character.name);
+      Promise.all(characterPromises).then(characters => {
+        characters.sort((a, b) => {
+          const getId = (url) => parseInt(url.split('/').slice(-2, -1)[0]);
+          return getId(a.url) - getId(b.url);
+        });
+
+        characters.forEach((character) => {
+          console.log(character.name);
+        });
+      }).catch(e => console.error(`Error fetching character data ${e}`));
     });
   } catch (e) {
     console.error(`Error fetching data: ${e}`);
   }
 };
 
-const fetchCharacterDetails = async (characterURL) => {
+const fetchCharacterDetails = (characterURL) => {
   // Function to fetch character details from a character URL
-  try {
-    const response = await request(characterURL);
-    // console.log(JSON.parse(response));
-    return JSON.parse(response);
-  } catch (e) {
-    console.error(`Error fetching character data: ${e}`);
-  }
+  return new Promise((resolve, reject) => {
+    request(characterURL, (error, response, body) => {
+      if (error) {
+        reject(error);
+      }
+      // console.log(JSON.parse(body));
+      const character = JSON.parse(body);
+      resolve(character);
+    });
+  });
 };
 
 const movieID = process.argv[2];
